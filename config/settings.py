c9731dbea8915b2aef30155f21fae8bcb59fce67
Muhammadxon2oo7 <muhammadxon0709@gmail.com>
2024-10-01 19:42:53 +0500

    scss removed

--- config/settings.py
+++ config/settings.py
@@ -164,6 +164,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Asosiy statik fayllar joylashuvi
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
